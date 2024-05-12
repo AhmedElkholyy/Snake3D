d8711a21e9f582530f1736bbb4b7b59ca42573bb
Ahmed Elkholy <ahmed.elkholy6480@gmail.com>
2024-05-12 07:16:09 +0300

    project

---
+++ Assets/Scripts/Helper Scripts/Tags.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tags : MonoBehaviour
+{
+    public static string FRUIT = "Fruit";
+    public static string TAIL = "Tail";
+    public static string BOMB = "Bomb";
+    public static string WALL = "Wall";
+
+}
+
+public class Metrics
+{
+    public static float NODE = 0.2f;
+}
+
+public enum PlayerDirection   //for easy access in lists
+
+{
+    LEFT = 0, UP = 1, RIGHT = 2, DOWN = 3, COUNT = 4
+
+
+
+
+}
